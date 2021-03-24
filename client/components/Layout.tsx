71dfc52d4989d5ff19170eebec66573d0de91ebd
jeff <jeffhogg86@gmail.com>
2021-03-24 15:48:52 -0700

    adds og metadata, thumbnail

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -14,6 +14,19 @@ const Layout = ({ children, title = 'CodeTech TV' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content={title} />
+      <meta
+        property="og:description"
+        content="Learn how to code with free coding videos"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://codetechtv.com/" />
+      <meta
+        property="og:image"
+        content="https://codetechtv.com/images/codetechhome.png"
+      />
     </Head>
     <Header />
     <main>{children}</main>
