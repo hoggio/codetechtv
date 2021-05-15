91d14fa8510710e5ab320c8d5161b176ee6e9528
jeff <jeffhogg86@gmail.com>
2021-05-15 01:46:12 -0700

    add ads

---
+++ client/components/Ads.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Ads = () => {
+  return (
+    <div>
+      <div id="amzn-assoc-ad-0f82280c-f07a-4ebf-bd57-5c59d2682c59"></div>
+      <script
+        async
+        src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=0f82280c-f07a-4ebf-bd57-5c59d2682c59"
+      ></script>
+    </div>
+  );
+};
+
+export default Ads;
--- client/pages/shop.tsx
+++ client/pages/shop.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Ads from '../components/Ads';
 
-const shopping = () => {
-  return <div>Shop</div>;
+const shop = () => {
+  return (
+    <div>
+      <Ads />
+    </div>
+  );
 };
 
-export default shopping;
+export default shop;
