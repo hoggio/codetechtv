779d2894176c9cec546fed935b91f0283a4a15d0
jeff <jeffhogg86@gmail.com>
2021-05-15 00:10:39 -0700

    Logo Link update

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -1,13 +1,12 @@
-import Link from './Link';
-import LinkButton from './LinkButton';
+import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 
 const Logo = () => {
   return (
-    <LinkButton variant="outlined" color="secondary" href="/">
+    <Link href="/">
       <Image src="/images/bluelogo.png" width={50} height={50} />
-    </LinkButton>
+    </Link>
   );
 };
 
