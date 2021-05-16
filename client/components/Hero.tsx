c00013b3c99a46239390e42f30307a0dedf2be58
jeff <jeffhogg86@gmail.com>
2021-05-15 17:08:39 -0700

    adds shop

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
           <h1 className="flex flex-col text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block xl:inline">CodeTech TV</span>{' '}
             <span className="block text-blue-700 xl:inline">
-              Watch. Code. Learn.
+              Watch. Code. Create.
             </span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
--- client/pages/shop.tsx
+++ client/pages/shop.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import Ads from '../components/Ads';
+import YoutubeList from '../components/YoutubeList';
 
 const shop = () => {
   return (
     <div>
-      <Ads />
+      <div className="bg-white mb-4">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Top YouTube Equipment
+            </p>
+            <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+              Start recording
+            </p>
+          </div>
+        </div>
+      </div>
+      <YoutubeList />
     </div>
   );
 };
