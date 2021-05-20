0f32010b913c368d177f15f6194dd3023998d0d1
jeff <jeffhogg86@gmail.com>
2021-05-19 22:42:23 -0700

    remove amazon banner

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -9,7 +9,6 @@ import {
   UserGroupIcon,
 } from '@heroicons/react/outline';
 import Logo from './Logo';
-import AmazonBanner from '../components/AmazonBanner';
 
 const navigation = [
   {
@@ -174,7 +173,6 @@ export default function Sidebar({ children }) {
           <div className="py-6">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
               {/* content */}
-              <AmazonBanner />
               {children}
             </div>
           </div>
